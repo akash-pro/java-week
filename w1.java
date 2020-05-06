d6f2f372d74f4c7b02f31f242ef688837d407311
Akash S <62745136+akash-pro@users.noreply.github.com>
2020-05-06 21:47:30 +0530

    w1.java

---
+++ w1.java
@@ -0,0 +1,30 @@
+class Engine {
+    boolean isOn;
+
+    void turnOn() {
+        // initialize variable with value true
+        isOn = true;
+        System.out.println("Engine on? " + isOn);
+
+    }
+
+    void turnOff() {
+        // initialize variable with value false
+        isOn = false;
+        System.out.println("Engine on? " + isOn);
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+  
+        // create objects l1 and l2
+        Engine l1 = new Engine();
+        Engine l2 = new Engine();
+  
+        // call methods turnOn() and turnOff()
+        l1.turnOn();
+        l2.turnOff();
+    }
+}
